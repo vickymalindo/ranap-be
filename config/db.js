3a0f9c9180df9e1a3a96ab86f726dd426483e3f3
vickymalindo <vicky.malindo@gmail.com>
2024-04-17 16:49:16 +0700

    'test'

--- config/db.js
+++ config/db.js
@@ -1,8 +1,25 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('MCSYS', 'admin_mcsys', 'mcsys_p@ssw0rd', {
-  host: '103.103.192.139,53055MCSYSSTANDBY',
+// const sequelize = new Sequelize('MCSYS', 'admin_mcsys', 'mcsys_p@ssw0rd', {
+//   host: String.raw`103.103.192.139,53055\MCSYSSTANDBY`,
+//   dialect: 'mssql',
+// });
+
+const sequelize = new Sequelize('database', null, null, {
   dialect: 'mssql',
+  dialectOptions: {
+    authentication: {
+      type: 'azure-active-directory-msi-vm',
+      options: {
+        domain: String.raw`103.103.192.139,53055\MCSYSSTANDBY`,
+        userName: 'admin_mcsys',
+        password: 'mcsys_p@ssw0rd',
+      },
+    },
+    // options: {
+    //   instanceName: 'MCSYSSTANDBY',
+    // },
+  },
 });
 
 export default sequelize;
